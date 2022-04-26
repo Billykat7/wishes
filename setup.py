ca7752359d695843a4fceb50bdd771017c9129cb
Billy Katalayi <billy@sbn.systems>
2022-04-26 07:35:21 +0200

    setup.py v0.0.10 updates for Pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools                         import setup, find_packages
 
 setup(
     name='realmz',
-    version='0.0.9',
+    version='0.0.10',
     license='MIT',
     author='Billy Katalayi',
     author_email='billysbn7@gmail.com',
@@ -11,21 +11,22 @@ setup(
     package_dir={'' : 'src'},
     url='https://github.com/Billykat7/wishes',
     keywords='employees birthday, anniversary automated wishes',
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-    ],
+    # classifiers=[
+    #     "License :: OSI Approved :: MIT License",
+    #     "Programming Language :: Python :: 3",
+    #     "Programming Language :: Python :: 3.9",
+    # ],
     install_requires=[
         # 'celery',
+        'python-dotenv',
         # 'redis',
         'requests',
         'SQLAlchemy'
     ],
-    entry_points={
-        "console_scripts": [
-            "realmz.__main__:main",
-        ]
-    },
-    include_package_data=True,
+    # entry_points={
+    #     "console_scripts": [
+    #         "realmz.__main__:main",
+    #     ]
+    # },
+    # include_package_data=True,
 )
